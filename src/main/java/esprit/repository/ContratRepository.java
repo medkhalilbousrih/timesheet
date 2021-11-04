cd0a2bfacb66c4b2b262c0b3e4991d22427f3b6e
khalil bousrih <medkhalilbousrih@gmail.com>
2021-11-04 16:50:35 +0100

    repo

---
+++ src/main/java/esprit/repository/ContratRepository.java
@@ -0,0 +1,10 @@
+package esprit.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import esprit.entities.Contrat;
+
+
+public interface ContratRepository extends CrudRepository<Contrat, Integer>{
+
+} 
