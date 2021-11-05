d1324826ae521696024e1746fb4e76d13b57acb2
khalil bousrih <medkhalilbousrih@gmail.com>
2021-11-05 03:47:56 +0100

    junit4

--- src/test/java/esprit/EntrepriseServiceTest.java
+++ src/test/java/esprit/EntrepriseServiceTest.java
@@ -2,9 +2,8 @@ package esprit;
 
 import static org.junit.Assert.assertNotEquals;
 import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertNull;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -16,25 +15,24 @@ import esprit.services.IEntrepriseService;
 
 @SpringBootTest
 @RunWith(SpringRunner.class)
-class EntrepriseServiceTest {
+public class EntrepriseServiceTest {
 	@Autowired
 	IEntrepriseService es;
-	
-	
+
 	@Test
 	public void testAjouterEntreprise() {
-		Entreprise e = new Entreprise("Poulina","anon");
-		assertNotEquals(0,es.ajouterEntreprise(e));
+		Entreprise e = new Entreprise("Poulina", "anon");
+		assertNotEquals(0, es.ajouterEntreprise(e));
 	}
-	
+
 	@Test
 	public void testAjouterDepartement() {
 		Departement d = new Departement("Finance");
 		Departement d1 = new Departement("Technique");
-		assertNotEquals(0,es.ajouterDepartement(d));
-		assertNotEquals(0,es.ajouterDepartement(d1));
+		assertNotEquals(0, es.ajouterDepartement(d));
+		assertNotEquals(0, es.ajouterDepartement(d1));
 	}
-	
+
 	@Test
 	public void testAffecterDepartementAEntreprise() {
 		assertNotNull(es.affecterDepartementAEntreprise(1, 1));
